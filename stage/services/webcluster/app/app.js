1abacbc6e1159072516a4449dbd6c1a5f2df7235
Matei Popa <mpopa@deloittece.com>
2018-01-09 12:30:50 +0200

    moved app to modules

--- stage/services/webcluster/app/app.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express()
-
-app.use(express.static('public'));
-app.get('/', (req, res) => res.redirect("/3.gif"));
-app.listen(9090, () => console.log('app listening on port 9090'))
