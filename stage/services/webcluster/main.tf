07b36f50bec6e57a8fdce8c35fa5815c490721d7
Matei Popa <mpopa@deloittece.com>
2018-01-09 17:35:24 +0200

    updating app_version_v2

--- stage/services/webcluster/main.tf
+++ stage/services/webcluster/main.tf
@@ -14,12 +14,3 @@ module "webcluster" {
     app_data_v2 = true
 }
 
-resource "aws_security_group_rule" "allow_apps_in" {
-     type = "ingress"                                                                                 
-     security_group_id = "${module.webcluster.elb_security_group_id}"                                 
-         from_port = 9090                                                                             
-         to_port = 9090
-     protocol = "tcp"                                                                                 
-     cidr_blocks = ["0.0.0.0/0"]                                                                      
-}  
-
